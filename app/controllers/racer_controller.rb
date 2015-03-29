3484c0cbff5763f75d8518570667d5737feeee8b
Merlyn <curious.attempt.bunny@gmail.com>
2015-03-29 10:18:03 -0700

    Add racer vs racer page.

--- app/controllers/racer_controller.rb
+++ app/controllers/racer_controller.rb
@@ -1,8 +1,23 @@
 class RacerController < ApplicationController
     def show
-        @racer = RacerRace.find_by(racer_id: params[:id].to_i)
-        @racer = RacerRace.find_by(nickname: params[:id].to_s) if @racer.nil?
+        @racer = resolve_racer_by_id(params[:id])
 
         @races_by_kart_type = RacerRace.where(racer_id: @racer.racer_id).order('kart_type, best_time asc').all.group_by(&:kart_type)
+    end
+
+    def versus
+        @racer1 = resolve_racer_by_id(params[:id1])
+        @racer2 = resolve_racer_by_id(params[:id2])
+
+        @races = RacerRace.where(racer_id: [@racer1.racer_id, @racer2.racer_id]).group_by(&:race_id)
+        @races.reject! { |k, racers| racers.size < 2 || racers.first.kart_type != racers.second.kart_type }
+    end
+
+    private
+
+    def resolve_racer_by_id(id)
+        racer = RacerRace.find_by(racer_id: id)
+        racer = RacerRace.find_by(nickname: id) if racer.nil?
+        racer
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get '/', to: redirect('/racer/Bunny%20GT9')
 
   get '/racer/:id' => 'racer#show'
+  get '/racer/:id1/vs/:id2' => 'racer#versus'
 end
