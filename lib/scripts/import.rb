2172b9cdee01d73d1c23872d2bfcf736d33c55e7
Merlyn <curious.attempt.bunny@gmail.com>
2015-04-01 19:38:18 -0700

    Fix import.

--- lib/scripts/import.rb
+++ lib/scripts/import.rb
@@ -1,12 +1,13 @@
 #!/usr/bin/env ruby
 
-Race.unimported[0...10].each do |race_id|
+Race.unimported.each do |race_id|
     race = Race.load(race_id)
+    puts race['id']
 
     ActiveRecord::Base.transaction do
         begin
             race["racers"].each do |racer|
-                if race['best_time']
+                if racer['best_time']
                     puts "starts_at: #{race['starts_at']} racer_id: #{racer['id']}, race_id: #{race_id} best_time: #{racer['best_time']} average_time: #{racer['average_time']}"
                     RacerRace.create!(racer_id: racer['id'], race_id: race_id, race_start: race['starts_at'], nickname: racer['nickname'], best_time: racer['best_time'], average_time: racer['average_time'], kart_number: racer['kart_number'], kart_type: racer['kart_type'])
                 end
